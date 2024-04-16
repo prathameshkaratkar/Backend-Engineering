f41356fd12f12d0e96cf4ff9429af63c368cfea2
Prathameshkaratkar <karatkarprathamesh@gmail.com>
2024-04-16 12:33:42 +0530

    promises acomplete

---
+++ 15 April/Promises/1.index.js
@@ -0,0 +1,20 @@
+// const cart = ["shoes", "pants", "kurta"]
+
+// createOrder(cart, function(orderId){
+//     proceedToPayment(orderId)
+// })
+
+
+
+// const promise = createOrder(cart);
+
+// // {data: undefined}
+
+// promise.then(function(orderId) {
+//     proceedToPayment(orderId);
+// })
+
+const GITHUB_API = "https://api.github.com/users/prathameshkaratkar"
+
+const user = fetch(GITHUB_API);
+
