777895b0d9f011ea8e2e117fb744d62cc24b86e9
Prathameshkaratkar <karatkarprathamesh@gmail.com>
2024-04-23 22:14:20 +0530

    implementing the rate limiting in the api

--- 19 April- Mongoose-Schema-Connect/src/index.js
+++ 19 April- Mongoose-Schema-Connect/src/index.js
@@ -1,16 +1,30 @@
 import express from 'express';
-
-const app = express();
-
 import mongoose from 'mongoose';
 import userRouter from './users/users.routes.js';
+import {rateLimit} from 'express-rate-limit';
 
-app.use(express.json());
-app.use('/users',userRouter);
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/myecommerce').then(() => 
     console.log("Database connected successfully"))
     .catch((err) => console.log("Database connection error: " + err))
+
+const app = express();
+app.use(express.json());
+
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: "Too many requests from this IP, please try again after 15 minutes"
+})
+
+
+
+
+
+app.use(limiter);
+app.use('/users',userRouter);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
